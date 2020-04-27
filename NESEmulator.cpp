70e97a006002e77af6191ffaf20004a3fe3a1b23
KingCodeFish <kingcodefish@gmail.com>
2020-04-26 20:23:10 -0400

    CMake Build System added with SDL2 in Debug mode for MSVC x64/x86

---
+++ NESEmulator.cpp
@@ -0,0 +1,49 @@
+#include "SDL.h"
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+
+#include "PPU.h"
+#include "CPU.h"
+#include "Cartridge.h"
+
+int main(int argc, char* argv[])
+{
+	SDL_Event evt;
+	bool running = true;
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window* window = SDL_CreateWindow
+	("NES Emulator", // window's title
+		100, 100, // coordinates on the screen, in pixels, of the window's upper left corner
+		256, 240, // window's length and height in pixels  
+		SDL_WINDOW_OPENGL);
+
+	std::string filename("C:\\Users\\Lucius\\Documents\\ROMs\\NES\\Super Mario Bros.nes");
+	//std::string filename("C:\\Users\\Lucius\\Documents\\ROMs\\NES\\nes-test.nes");
+
+	Cartridge::load(filename.c_str());
+
+	PPU::initialize();
+	CPU::power();
+
+	/*while(running)
+	{
+		SDL_PollEvent(&evt);
+		switch(evt.type)
+		{
+		case SDL_QUIT:
+			running = false;
+			break;
+		}
+	}*/
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
